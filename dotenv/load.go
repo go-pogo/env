5a2ce8b639d0b41cc063c3d8e30dff861f29875a
roeldev <roeldev@users.noreply.github.com>
2023-07-14 20:41:20 +0200

    Add dotenv Load and Overload functions

---
+++ dotenv/load.go
@@ -0,0 +1,39 @@
+// Copyright (c) 2023, Roel Schut. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package dotenv
+
+import (
+	"io/fs"
+)
+
+func Load(dir string, ae Environment) error {
+	return readAndLoad(Read(dir, ae), false)
+}
+
+func Overload(dir string, ae Environment) error {
+	return readAndLoad(Read(dir, ae), true)
+}
+
+func LoadFS(fsys fs.FS, ae Environment) error {
+	return readAndLoad(ReadFS(fsys, ae), false)
+}
+
+func OverloadFS(fsys fs.FS, ae Environment) error {
+	return readAndLoad(ReadFS(fsys, ae), true)
+}
+
+func readAndLoad(r *Reader, overload bool) error {
+	defer r.Close()
+	m, err := r.Map()
+	if err != nil {
+		return err
+	}
+
+	if overload {
+		return m.Overload()
+	} else {
+		return m.Load()
+	}
+}
