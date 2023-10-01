4e63c8e80c588b2b6c318cd83a4dcc0522d058f8
roeldev <roeldev@users.noreply.github.com>
2023-10-01 23:15:10 +0200

    Update tests

---
+++ dotenv/examples_test.go
@@ -0,0 +1,12 @@
+package dotenv
+
+//func ExampleLoadFS() {
+//	LoadFS(os.DirFS("."), Development, false)
+//}
+//
+//func ExampleReadFS() {
+//	var dest struct{}
+//	dec := env.NewDecoder(ReadFS(os.DirFS("."), Development))
+//	dec.ReplaceVars = true
+//	dec.Decode(&dest)
+//}
