ff65cdf4665e381f004fee4e948e180b5d728f9c
roeldev <roeldev@users.noreply.github.com>
2024-02-05 19:54:23 +0100

    Update ActiveEnvironment

---
+++ dotenv/environment.go
@@ -0,0 +1,62 @@
+// Copyright (c) 2022, Roel Schut. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package dotenv
+
+import (
+	"strings"
+)
+
+const (
+	None        ActiveEnvironment = ""
+	Development ActiveEnvironment = "dev"
+	Testing     ActiveEnvironment = "test"
+	Production  ActiveEnvironment = "prod"
+
+	LongFlag = "active-env"
+
+	flag1 = "-" + LongFlag
+	flag2 = "--" + LongFlag
+)
+
+// ActiveEnvironment is the active environment.
+type ActiveEnvironment string
+
+func (ae ActiveEnvironment) String() string { return string(ae) }
+
+// GetActiveEnvironment returns the active environment and the remaining
+// arguments from the provided args.
+//
+//	args := os.Args[1:]
+//	env, args := dotenv.GetActiveEnvironment(args...)
+func GetActiveEnvironment(args ...string) (ActiveEnvironment, []string) {
+	const flag1a = flag1 + "="
+	const flag2a = flag2 + "="
+	const flag1b = len(flag1a)
+	const flag2b = len(flag2a)
+
+	for i, l := 0, len(args)-1; i <= l; i++ {
+		arg := args[i]
+		if arg == "" {
+			continue
+		}
+		if arg == flag1 || arg == flag2 {
+			if i == l {
+				// at end of args, no value set
+				return "", args[:]
+			}
+
+			arg = args[i+1]
+			return ActiveEnvironment(arg), append(args[:i], args[i+2:]...)
+		}
+		if strings.HasPrefix(arg, flag1a) {
+			return ActiveEnvironment(arg[flag1b:]), append(args[:i], args[i+1:]...)
+		}
+		if strings.HasPrefix(arg, flag2a) {
+			return ActiveEnvironment(arg[flag2b:]), append(args[:i], args[i+1:]...)
+		}
+	}
+
+	return None, args
+}
---
+++ dotenv/environment_test.go
@@ -0,0 +1,74 @@
+// Copyright (c) 2023, Roel Schut. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package dotenv
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestGetActiveEnvironment(t *testing.T) {
+	type testCase struct {
+		args       []string
+		wantEnv    ActiveEnvironment
+		wantRemain []string
+	}
+	tests := map[string]testCase{
+		"empty": {},
+		"none": {
+			args:       []string{"-test", flag1 + "x", flag2 + "y"},
+			wantRemain: []string{"-test", flag1 + "x", flag2 + "y"},
+		},
+		"only flag1": {
+			args:       []string{flag1, "test"},
+			wantEnv:    Testing,
+			wantRemain: []string{},
+		},
+		"only flag2": {
+			args:       []string{flag2, "test"},
+			wantEnv:    Testing,
+			wantRemain: []string{},
+		},
+		"flag1 only arg": {
+			args:       []string{flag1 + "=dev"},
+			wantEnv:    Development,
+			wantRemain: []string{},
+		},
+		"flag2 only arg": {
+			args:       []string{flag2 + "=prod"},
+			wantEnv:    Production,
+			wantRemain: []string{},
+		},
+		"flag1 at start with remain": {
+			args:       []string{flag1, "dev", "-remainder"},
+			wantEnv:    Development,
+			wantRemain: []string{"-remainder"},
+		},
+		"flag2 at start with remain": {
+			args:       []string{flag2, "dev", "-remainder", flag1},
+			wantEnv:    Development,
+			wantRemain: []string{"-remainder", flag1},
+		},
+		"at end with remain": {
+			args:       []string{"-remainder", flag1, "prod"},
+			wantEnv:    Production,
+			wantRemain: []string{"-remainder"},
+		},
+		"at end no value": {
+			args:       []string{"-remainder", flag1},
+			wantRemain: []string{"-remainder", flag1},
+		},
+	}
+
+	for name, tc := range tests {
+		t.Run(name, func(t *testing.T) {
+			argsLen := len(tc.args)
+			haveEnv, haveRemain := GetActiveEnvironment(tc.args...)
+			assert.Equal(t, tc.wantEnv, haveEnv)
+			assert.Equal(t, tc.wantRemain, haveRemain)
+			assert.Len(t, tc.args, argsLen, "args length should not change")
+		})
+	}
+}
