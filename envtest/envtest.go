60c6cf26c04a85e283f61d1e0cfa6b45d415ab53
roeldev <roeldev@users.noreply.github.com>
2024-02-27 20:12:30 +0100

    Add envtest subpkg

---
+++ envtest/envtest.go
@@ -0,0 +1,34 @@
+// Copyright (c) 2024, Roel Schut. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package envtest
+
+import (
+	"github.com/go-pogo/env"
+	"os"
+)
+
+type Snapshot struct {
+	envs env.Map
+}
+
+func Prepare(testenvs env.Map) *Snapshot {
+	r := Snapshot{envs: env.Environ()}
+	os.Clearenv()
+
+	if testenvs != nil && len(testenvs) > 0 {
+		if err := env.Load(testenvs); err != nil {
+			panic(err)
+		}
+	}
+
+	return &r
+}
+
+func (r *Snapshot) Restore() {
+	os.Clearenv()
+	if err := env.Load(r.envs); err != nil {
+		panic(err)
+	}
+}
