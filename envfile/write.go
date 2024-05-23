06a003cdc4f7191a6599ae7b76fb831883b4bc13
roeldev <roeldev@users.noreply.github.com>
2024-05-23 18:02:25 +0200

    Add docs

--- envfile/write.go
+++ envfile/write.go
@@ -15,11 +15,14 @@ var _ io.Closer = (*Encoder)(nil)
 
 type encoder = env.Encoder
 
+// Encoder embeds an env.Encoder and sets its target io.Writer to an os.File.
 type Encoder struct {
 	*encoder
 	file *os.File
 }
 
+// NewEncoder returns a new Encoder which writes the encoded values to the
+// provided os.File f.
 func NewEncoder(f *os.File) *Encoder {
 	if f == nil {
 		panic(panicNilFile)
@@ -30,6 +33,8 @@ func NewEncoder(f *os.File) *Encoder {
 	}
 }
 
+// Create returns a new Encoder which creates the file filename and uses it to
+// write the encoded values to.
 func Create(filename string) (*Encoder, error) {
 	f, err := os.Create(filename)
 	if err != nil {
@@ -38,7 +43,9 @@ func Create(filename string) (*Encoder, error) {
 	return NewEncoder(f), nil
 }
 
-func Write(filename string, v any) error {
+// Write creates the file filename using Create, writes the encoded value v
+// to it and closes its internal file handle.
+func Write(filename string, v any) (err error) {
 	enc, err := Create(filename)
 	if err != nil {
 		return err
@@ -46,7 +53,7 @@ func Write(filename string, v any) error {
 	return enc.Encode(v)
 }
 
-// Close closes the underlying os.File.
+// Close closes its internal os.File.
 func (fe *Encoder) Close() error {
 	return errors.WithStack(fe.file.Close())
 }
